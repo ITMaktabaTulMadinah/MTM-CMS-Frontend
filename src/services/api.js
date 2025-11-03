49ae5c8df08ed9bcbf09595442e1aadae872220a
ITMaktabaTulMadinah <asst.it@maktabatulmadinah>
2025-11-03 15:46:22 +0500

    backend url update

--- src/services/api.js
+++ src/services/api.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const API_BASE_URL =
   import.meta.env.VITE_API_BASE_URL ||
   "http://localhost:5000/api" ||
-  "https://mtm-cms-backend.vercel.app/";
+  "https://mtm-cms-backend.vercel.app/api";
 
 // Create axios instance
 const api = axios.create({
